7c41ff7cc9e9b06d6391b5259446aed66ca24e48
jwolffteach <jwolff@dsdmail.net>
2018-07-11 11:17:53 -0600

    Moved valves templates to /valves

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,28 +3,6 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User, ZoneSchedule
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
-    
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -39,21 +17,12 @@ class EditProfileForm(FlaskForm):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
-                raise ValidationError('Please use a different username.')
+                raise ValidationError(_('Please use a different username.'))
+
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)])
+    post = TextAreaField('Say something', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
-class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Request Password Reset')
 
 class ValveForm(FlaskForm):
     valve = TextAreaField('Valve Number', validators=[DataRequired(), Length(min=1, max=2)])
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,24 +1,23 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug.urls import url_parse
-from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
-    ResetPasswordRequestForm, ResetPasswordForm, ValveForm, EditValveForm, \
+from flask import render_template, flash, redirect, url_for, request, g, \
+    current_app
+from flask_login import current_user, login_required
+from app import db
+from app.main.forms import EditProfileForm, PostForm, ValveForm, EditValveForm, \
     ZoneScheduleForm, EditZoneScheduleForm
 from app.models import User, Post, Valve, ZoneSchedule
-from app.email import send_password_reset_email
+from app.main import bp
 
 
-@app.before_request
+@bp.before_app_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
@@ -27,119 +26,50 @@ def index():
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('index', page=posts.next_num) \
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.index', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
+    prev_url = url_for('main.index', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title='Home', form=form,
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
 
 
-@app.route('/explore')
+@bp.route('/explore')
 @login_required
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('explore', page=posts.next_num) \
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.explore', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) \
+    prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
+    return render_template('index.html', title='Explore',
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/reset_password_request', methods=['GET', 'POST'])
-def reset_password_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = ResetPasswordRequestForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user:
-            send_password_reset_email(user)
-        flash('Check your email for the instructions to reset your password')
-        return redirect(url_for('login'))
-    return render_template('reset_password_request.html',
-                           title='Reset Password', form=form)
-
-
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
-def reset_password(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    user = User.verify_reset_password_token(token)
-    if not user:
-        return redirect(url_for('index'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.set_password(form.password.data)
-        db.session.commit()
-        flash('Your password has been reset.')
-        return redirect(url_for('login'))
-    return render_template('reset_password.html', form=form)
-
-
-@app.route('/user/<username>')
+@bp.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
-        if posts.has_prev else None
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.user', username=user.username,
+                       page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.user', username=user.username,
+                       page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
 
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm(current_user.username)
@@ -148,7 +78,7 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('main.edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
@@ -156,38 +86,38 @@ def edit_profile():
                            form=form)
 
 
-@app.route('/follow/<username>')
+@bp.route('/follow/<username>')
 @login_required
 def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        flash('User %(username)s not found.', username=username)
+        return redirect(url_for('main.index'))
     if user == current_user:
-        flash('You cannot follow yourself!')
-        return redirect(url_for('user', username=username))
+        flash(_('You cannot follow yourself!'))
+        return redirect(url_for('main.user', username=username))
     current_user.follow(user)
     db.session.commit()
-    flash('You are following {}!'.format(username))
-    return redirect(url_for('user', username=username))
+    flash(_('You are following %(username)s!', username=username))
+    return redirect(url_for('main.user', username=username))
 
 
-@app.route('/unfollow/<username>')
+@bp.route('/unfollow/<username>')
 @login_required
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        flash('User %(username)s not found.', username=username)
+        return redirect(url_for('main.index'))
     if user == current_user:
         flash('You cannot unfollow yourself!')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
-    flash('You are not following {}.'.format(username))
-    return redirect(url_for('user', username=username))
+    flash('You are not following %(username)s.', username=username)
+    return redirect(url_for('main.user', username=username))
 
-@app.route('/valves', methods=['GET', 'POST'])
+@bp.route('/valves', methods=['GET', 'POST'])
 @login_required
 def valves():
     form = ValveForm()
@@ -196,10 +126,10 @@ def valves():
         db.session.add(valve)
         db.session.commit()
         flash('Your valve has been added!')
-        return redirect(url_for('valves'))
+        return redirect(url_for('main.valves'))
     page = request.args.get('page', 1, type=int)
     valves = Valve.query.order_by(Valve.id).paginate(
-        page, app.config['VALVES_PER_PAGE'], False)
+        page, current_app.config['VALVES_PER_PAGE'], False)
     next_url = url_for('valves', page=valves.next_num) \
         if valves.has_next else None
     prev_url = url_for('valves', page=valves.prev_num) \
@@ -208,7 +138,7 @@ def valves():
                            valves=valves.items, next_url=next_url,
                            prev_url=prev_url)
 
-@app.route('/edit_valve/<valve>', methods=['GET', 'POST'])
+@bp.route('/edit_valve/<valve>', methods=['GET', 'POST'])
 @login_required
 def edit_valve(valve):
     form = EditValveForm(valve)
@@ -220,7 +150,7 @@ def edit_valve(valve):
         oldValve.gpio_pin = form.gpio_pin.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('valves'))
+        return redirect(url_for('main.valves'))
     elif request.method == 'GET':
         form.id.data = oldValve.id
         form.valve.data = oldValve.valve
@@ -229,12 +159,12 @@ def edit_valve(valve):
     return render_template('edit_valve.html', title='Edit Valve',
                            form=form)
 
-@app.route('/schedule', methods=['GET', 'POST'])
+@bp.route('/schedule', methods=['GET', 'POST'])
 @login_required
 def schedule():
     page = request.args.get('page', 1, type=int)
     zoneSchedules = ZoneSchedule.query.order_by(ZoneSchedule.id).paginate(
-        page, app.config['VALVES_PER_PAGE'], False)
+        page, current_app.config['VALVES_PER_PAGE'], False)
     next_url = url_for('schedule', page=zoneSchedules.next_num) \
         if zoneSchedules.has_next else None
     prev_url = url_for('schedule', page=zoneSchedules.prev_num) \
@@ -243,7 +173,7 @@ def schedule():
                            zoneSchedules=zoneSchedules.items, next_url=next_url,
                            prev_url=prev_url)
 
-@app.route('/add_schedule', methods=['GET', 'POST'])
+@bp.route('/add_schedule', methods=['GET', 'POST'])
 @login_required
 def add_schedule():
     form = ZoneScheduleForm()
@@ -254,10 +184,10 @@ def add_schedule():
         db.session.add(zoneSchedule)
         db.session.commit()
         flash('Your valve schedule has been added!')
-        return redirect(url_for('schedule'))
+        return redirect(url_for('main.schedule'))
     return render_template('add_schedule.html', title='Schedule a Zone', form=form)
 
-@app.route('/edit_schedule/<schedule>', methods=['GET', 'POST'])
+@bp.route('/edit_schedule/<schedule>', methods=['GET', 'POST'])
 @login_required
 def edit_schedule(schedule):
     form = EditZoneScheduleForm(schedule)
@@ -268,7 +198,7 @@ def edit_schedule(schedule):
         oldSchedule.runLength = form.runLength.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('schedule'))
+        return redirect(url_for('main.schedule'))
     elif request.method == 'GET':
         form.id.data = oldSchedule.id
         form.zone.data = oldSchedule.zone
