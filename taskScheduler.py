7361a9f520d27fad8069f91a700bafd3c8a4dbfe
Jeremy Wolff <jwolff@dsdmail.net>
2018-07-19 12:12:32 -0600

    Updated taskscheduler.py include days
    RunSprinklers now takes into account the daily schedule. Monday, Tuesday, Wednesday, etc.

--- taskScheduler.py
+++ taskScheduler.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+import calendar
 import os
 import RPi.GPIO as GPIO
 from flask import current_app, render_template
@@ -33,8 +34,8 @@ def run_sprinklers(app):
         
         # This query gives us the scheduled zones:
         # id, valve, gpio_pin, and runLength
-        scheduledZones = ZoneSchedule.query.join(Valve, ZoneSchedule.zone == Valve.valve).add_columns(Valve.valve, Valve.description, Valve.gpio_pin, ZoneSchedule.runLength)
-
+        scheduledZones = ZoneSchedule.query.join(Valve, ZoneSchedule.zone == Valve.valve).add_columns(Valve.valve, Valve.description, Valve.gpio_pin, ZoneSchedule.runLength, ZoneSchedule.m, ZoneSchedule.t, ZoneSchedule.w, ZoneSchedule.th, ZoneSchedule.f, ZoneSchedule.s, ZoneSchedule.su)
+        currentDay = calendar.day_name[datetime.now().weekday()] # GIves Monday, Tuesday, Wednesday, etc.
         GPIO.setmode(GPIO.BCM)
 
         # Prepare all valves
@@ -45,31 +46,40 @@ def run_sprinklers(app):
         
         try:
             for zone in scheduledZones:
-                if(os.environ["SPRINKLERS_STATUS"] == "READY"):
-                    running = True
-                    progressSec = 0
-                    progressPercent = 0.0
-                    zoneRunLength = zone.runLength * 60 # Num Seconds this Zone will run
-                    GPIO.output(zone.gpio_pin, GPIO.LOW)
-                    writeLog(str(datetime.now())+': '+str(zone.valve)+ " "+ zone.description+ " is running on GPIO pin "+ str(zone.gpio_pin)+ " for "+ str(zone.runLength) + " minutes.")
-                    while running: # Every minute, check if sprinkler status is set to ready.
-                        if(os.environ["SPRINKLERS_STATUS"] == "READY" and progressSec < zoneRunLength):
-                            time.sleep(10) # Sleep for 10 seconds.
-                            progressSec += 10
-                            progressPercent = progressSec/zoneRunLength
-                            print(str(datetime.now())+': '+str(zone.valve)+ " "+ zone.description+': '+"Current Progress: "+"{:.2%}".format(progressPercent))
-                            writeLog(str(datetime.now())+': '+str(zone.valve)+ " "+ zone.description+": Current Progress: "+"{:.2%}".format(progressPercent))
-                        else:
-                            running = False
-                            progressSec = 0
-                    GPIO.output(zone.gpio_pin, GPIO.HIGH)
-                    writeLog(str(datetime.now())+': '+str(zone.valve)+' is off.')
-                    time.sleep(5)
-                else:
-                    #app.logger.info('SPRINKLER_STATUS: ', current_app.config['SPRINKLERS_STATUS'])
-                    break
-            GPIO.cleanup()
-            writeLog(str(datetime.now())+': '+'Cleaning Up!')
+                if((currentDay=="Monday" and zone.m)
+                or (currentDay=="Tuesday" and zone.t)
+                or (currentDay=="Wednesday" and zone.w)
+                or (currentDay=="Thursday" and zone.th)
+                or (currentDay=="Friday" and zone.f)
+                or (currentDay=="Saturday" and zone.s)
+                or (currentDay=="Sunday" and zone.su)
+                ):
+
+                    if(os.environ["SPRINKLERS_STATUS"] == "READY"):
+                        running = True
+                        progressSec = 0
+                        progressPercent = 0.0
+                        zoneRunLength = zone.runLength * 60 # Num Seconds this Zone will run
+                        GPIO.output(zone.gpio_pin, GPIO.LOW)
+                        writeLog(str(datetime.now())+': '+str(zone.valve)+ " "+ zone.description+ " is running on GPIO pin "+ str(zone.gpio_pin)+ " for "+ str(zone.runLength) + " minutes.")
+                        while running: # Every minute, check if sprinkler status is set to ready.
+                            if(os.environ["SPRINKLERS_STATUS"] == "READY" and progressSec < zoneRunLength):
+                                time.sleep(10) # Sleep for 10 seconds.
+                                progressSec += 10
+                                progressPercent = progressSec/zoneRunLength
+                                print(str(datetime.now())+': '+str(zone.valve)+ " "+ zone.description+': '+"Current Progress: "+"{:.2%}".format(progressPercent))
+                                writeLog(str(datetime.now())+': '+str(zone.valve)+ " "+ zone.description+": Current Progress: "+"{:.2%}".format(progressPercent))
+                            else:
+                                running = False
+                                progressSec = 0
+                        GPIO.output(zone.gpio_pin, GPIO.HIGH)
+                        writeLog(str(datetime.now())+': '+str(zone.valve)+' is off.')
+                        time.sleep(5)
+                    else:
+                        #app.logger.info('SPRINKLER_STATUS: ', current_app.config['SPRINKLERS_STATUS'])
+                        break
+                GPIO.cleanup()
+                writeLog(str(datetime.now())+': '+'Cleaning Up!')
 
         except:
             print("  Quit")
@@ -87,6 +97,7 @@ def stop_all_sprinklers(app):
         GPIO.setmode(GPIO.BCM)
         valves = Valve.query.all()
         for valve in valves:
+            print("reseting valves...")
             GPIO.setup(valve.gpio_pin, GPIO.OUT)
             GPIO.output(valve.gpio_pin, GPIO.HIGH)
         print("Cleaning up.")
